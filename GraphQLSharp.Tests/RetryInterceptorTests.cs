7716718171486290093adc2653f5a24700eb9783
Clement <clement.gutel@gmail.com>
2025-07-28 21:34:13 +1000

    RetryInterceptorTests

--- GraphQLSharp.Tests/RetryInterceptorTests.cs
+++ GraphQLSharp.Tests/RetryInterceptorTests.cs
@@ -4,13 +4,37 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace GraphQLSharp.Tests;
 
 [TestClass]
-public class InterceptorTests
+public class RetryInterceptorTests
 {
-    private GraphQLCLient _client = new();
-
-    [TestInitialize]
-    public void Initialize()
+    [TestMethod]
+    public async Task NoRetryOnSuccess()
     {
+        var query = """
+            {
+                shop
+                {
+                    id
+                }
+            }
+            """;
+
+        int tries = 0;
+        var options = new GraphQLClientOptions
+        {
+            Uri = new Uri("https://example.com/graphql"),
+            Interceptor = new RetryInterceptor(),
+            HttpClient = new HttpClient(new TestHttpMessageHandler(msg =>
+            {
+                ++tries;
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new StringContent("""{"data":{"shop":{"id":"gid://shopify/Shop/1234567890"}}}""")
+                };
+            }))
+        };
+        var response = await new GraphQLCLient().ExecuteAsync(query, options);
+        Assert.AreEqual(1, tries);
+        Assert.AreEqual(HttpStatusCode.OK, response.HttpResponse.StatusCode);
     }
 
     [TestMethod]
@@ -39,9 +63,8 @@ public class InterceptorTests
                 };
             }))
         };
-        var response = await _client.ExecuteAsync(query, options);
+        var response = await new GraphQLCLient().ExecuteAsync(query, options);
         Assert.AreEqual(RetryInterceptor.MAX_TRIES, tries);
         Assert.AreEqual(HttpStatusCode.OK, response.HttpResponse.StatusCode);
     }
-
 }
