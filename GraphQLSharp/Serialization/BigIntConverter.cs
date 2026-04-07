5289739b4493fbd0eb2837d17e2e616b069f69f3
matnun-br <mathieu@betterreports.com>
2026-04-07 11:00:44 +1000

    Revert "Fix BigIntConverter: use Utf8JsonReader APIs, invariant culture, and JsonException on failure"
    This reverts commit a749d312e39118730d0561a0123a11c94279dab3.

--- GraphQLSharp/Serialization/BigIntConverter.cs
+++ GraphQLSharp/Serialization/BigIntConverter.cs
@@ -1,78 +1,26 @@
-using System.Globalization;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-public class LongConverter : JsonConverter<long>
+public class LongConverter : BigIntConverter<long>
 {
-    public override long Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        if (reader.TokenType == JsonTokenType.String)
-        {
-            var str = reader.GetString();
-            if (long.TryParse(str, NumberStyles.Integer, CultureInfo.InvariantCulture, out var result))
-                return result;
-            throw new JsonException($"Unable to parse '{str}' as a long.");
-        }
-
-        if (reader.TokenType == JsonTokenType.Number)
-            return reader.GetInt64();
-
-        throw new JsonException($"Unexpected token type {reader.TokenType} when parsing long.");
-    }
-
-    public override void Write(Utf8JsonWriter writer, long value, JsonSerializerOptions options)
-    {
-        writer.WriteStringValue(value.ToString(CultureInfo.InvariantCulture));
-    }
 }
 
-public class ULongConverter : JsonConverter<ulong>
+public class ULongConverter : BigIntConverter<ulong>
 {
-    public override ulong Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-    {
-        if (reader.TokenType == JsonTokenType.String)
-        {
-            var str = reader.GetString();
-            if (ulong.TryParse(str, NumberStyles.Integer, CultureInfo.InvariantCulture, out var result))
-                return result;
-            throw new JsonException($"Unable to parse '{str}' as a ulong.");
-        }
-
-        if (reader.TokenType == JsonTokenType.Number)
-            return reader.GetUInt64();
-
-        throw new JsonException($"Unexpected token type {reader.TokenType} when parsing ulong.");
-    }
-
-    public override void Write(Utf8JsonWriter writer, ulong value, JsonSerializerOptions options)
-    {
-        writer.WriteStringValue(value.ToString(CultureInfo.InvariantCulture));
-    }
 }
 
-[Obsolete("Use LongConverter or ULongConverter directly.")]
 public abstract class BigIntConverter<T> : JsonConverter<T>
 {
     public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         if (reader.TokenType == JsonTokenType.String)
-        {
-            var str = reader.GetString();
-            try
-            {
-                return (T)Convert.ChangeType(str, typeof(T), CultureInfo.InvariantCulture);
-            }
-            catch (Exception ex)
-            {
-                throw new JsonException($"Unable to parse '{str}' as {typeof(T).Name}.", ex);
-            }
-        }
+            return (T)Convert.ChangeType(reader.GetString(), typeof(T));
 
-        throw new JsonException($"Unexpected token type {reader.TokenType} when parsing {typeof(T).Name}.");
+        return JsonSerializer.Deserialize<T>(ref reader);
     }
 
     public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
     {
-        writer.WriteStringValue(Convert.ToString(value, CultureInfo.InvariantCulture));
+        writer.WriteStringValue(value.ToString());
     }
 }
